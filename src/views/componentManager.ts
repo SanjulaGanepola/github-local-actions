85d4c038b7603875e7a35c59850084c13c54c0ad
Sanjula Ganepola <sanjulagane@gmail.com>
2024-09-25 20:41:06 -0400

    Fix to github cli
    Signed-off-by: Sanjula Ganepola <sanjulagane@gmail.com>

--- src/views/componentManager.ts
+++ src/views/componentManager.ts
@@ -19,10 +19,10 @@ export class ComponentManager {
             message: 'GitHub Actions extension is not required but is recommended to take advantage of workflow editor features'
         },
         {
-            name: 'GitHub Actions CLI',
+            name: 'GitHub CLI',
             status: Status.Warning,
             icon: 'terminal',
-            message: 'GitHub Actions CLI is not required but is recommended if you plan to use it to retrieve GitHub tokens'
+            message: 'GitHub CLI is not required but is recommended if you plan to use it to retrieve GitHub tokens'
         }
     ];
 
