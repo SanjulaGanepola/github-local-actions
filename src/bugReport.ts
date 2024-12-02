4fecbc0450d1df8236cde4e53a55df6763f57d36
Sanjula Ganepola <sanjulagane@gmail.com>
2024-12-01 19:11:00 -0500

    Init work for creating bug report
    Signed-off-by: Sanjula Ganepola <sanjulagane@gmail.com>

---
+++ src/bugReport.ts
@@ -0,0 +1,7 @@
+export class BugReport {
+    static baseUrl: string = 'https://github.com/SanjulaGanepola/github-local-actions/issues/new?assignees=&labels=bug&projects=&template=1-bug_report.yml';
+
+    staticOpenBugReportIssue() {
+
+    }
+}
